9af1e8bb00138058b0790cae48fa56d071070d24
Bernardo Busatto <berbusatto@gmail.com>
2023-01-06 16:41:03 -0300

    redirecionamento de rotas, verificacao de nome, soft delete, implicit bind

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,12 +7,14 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+    // RETORNA A BLADE DO INDEX
     public function index()
     {
         $users = User::get();
-        return view('user.index', ['users' => $users]); // DENTRO DE RESOURCES/VIEWS
+        return view('user.index', ['users' => $users]);
     }
 
+    // RETORNA A BLADE DO NOVO CONTATO
     public function create()
     {
         return view('user.create');
@@ -21,24 +23,33 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $data = $request->only(['name', 'email']);
-        $data['password'] = bcrypt(rand(12345678, 987654321));
+        $data['password'] = bcrypt(rand(12345678, 87654321));
         User::create($data);
-
 
         return redirect()->route('user.index');
     }
 
-    public function show($id)
+//    public function show($id)
+//    {
+//        // RETORNA OBJETO
+//        $user = User::find($id);
+//
+//        // RETORNA ARRAY
+//        //$user = User::where('id', $id)->get();
+//
+//        return view('user.show', ['user'=> $user]);
+//    }
+
+
+    // SEMELHANTE AO MÉTODO DE CIMA PORÉM COM BIND IMPLICIT E INJEÇÃO DE DEPENDENCIA
+    // INJETAR A CLASSE NOS PARAMETROS E A VARIÁVEL DEVE TER O MESMO NOME QUE ESTÁ NO PARAM DA ROTA
+    // DESTA FORMA O LARAVEL ENTENDE QUE ESTAMOS BUSCANDO UM USUÁRIO E AUTOMATIZA O PROCESSO
+    public function show(User $user)
     {
-        // RETORNA OBJETO
-        $user = User::find($id);
-
-        // RETORNA ARRAY
-        //$user = User::where('id', $id)->get();
-
-        return view('user.show', ['user'=> $user]);
+        return view('user.show', [
+            'user' => $user
+        ]);
     }
-
 
     public function edit($id)
     {
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users/add',[UserController::class, 'create'])->name('user.create');
-Route::post('/users/add', [UserController::class, 'store'])->name('user.store');
-Route::post('/users/add', [UserController::class, 'store'])->name('user.store');
-Route::get('/users', [UserController::class, 'index'])->name('user.index');
-Route::get('/users/{id?}', [UserController::class, 'show'])->name('user.show');
-Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
-Route::put('/users/update/{id}', [UserController::class, 'update'])->name('user.update');
-Route::delete('/users/delete{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+// SINTAXE DAS ROTAS ANTES DO AGRUPAMENTO(PREFIX, NAME, CONTROLLER)
+// Route::get('/users', [UserController::class, 'index'])->name('user.index');
+
+
+// ROTA PARA IMPLEMENTAR UM IMPLICIT BIND COM INJEÇÃO DE DEPENDENCIA NO MÉTODO SHOW
+// BUSCANDO PELO EMAIL. POR PADRÃO {/user} ELE BUSCA POR ID
+// DUPLICADA!!  REPETE ABAIXO DENTRO DO GROUP
+// Route::get('/users/{user:email}', [UserController::class, 'show'])->name('user.show');
+
+Route::prefix('users')
+    ->name('user.')
+    ->controller(UserController::class)
+    ->group(function (){
+        Route::get('/', 'index')->name('index');
+        Route::get('/add','create')->name('create');
+        Route::get('/{user}', 'show')->name('show');
+        Route::get('/edit/{id}', 'edit')->name('edit');
+        Route::post('/add', 'store')->name('store');
+        Route::put('/update/{id}', 'update')->name('update');
+        Route::delete('/delete{id}', 'destroy')->name('destroy');
+});
+
+
